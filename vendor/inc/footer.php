fd1d25dc821043d7427056ffe26e371a81fc3882
Jockthan <jasminblinkz@gmail.com>
2023-06-30 10:25:15 +0100

    Structured Database home and database

---
+++ vendor/inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; <?php echo date('Y');?> TravelBetter <i class ="fa fa-heartbeat"></i> By Elysium   
+       </p>
+    </div>
+    <!-- /.container -->
+  </footer>
